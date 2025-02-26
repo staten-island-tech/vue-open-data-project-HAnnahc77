499468ac8ed437e75dfe2bd01932d5e4d8af9500
HAnnahc77 <HAnnahc77@nycstudents.net>
2025-02-26 14:00:54 -0500

    commit

---
+++ vue-open-data/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
