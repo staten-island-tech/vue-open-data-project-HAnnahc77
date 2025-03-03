1bf3dc62dcbe79a1cc14948759086c49769eeec3
HAnnahc77 <HAnnahc77@nycstudents.net>
2025-03-03 14:01:03 -0500

    xxx

--- vue-open-data/src/views/HomeView.vue
+++ vue-open-data/src/views/HomeView.vue
@@ -1,7 +1,27 @@
 <template>
-  <div></div>
+  <main>
+    <SquirrelCard v-for="squirrel in squirrels" :key="squirrel.name" :squirrel="squirrels" />
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const squirrel = ref('')
+
+async function getData() {
+  let result = await fetch(
+    `https://data.cityofnewyork.us/resource/vfnx-vebw.json/${route.params.id}`,
+  )
+  let data = await result.json()
+  squirrel.value = data
+}
+
+onMounted(() => {
+  getData()
+})
+</script>
 
 <style lang="scss" scoped></style>
